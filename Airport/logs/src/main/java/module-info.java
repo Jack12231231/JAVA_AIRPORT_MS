5798f85e30d96b1861ba84cacce1d053b203e2d0
Jack12231231 <gleb28032007@gmail.com>
2026-03-24 21:31:46 +0300

    Delete Airport directory

--- Airport/logs/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.logs {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.logs to javafx.fxml;
-    exports com.example.logs;
-}
--- Airport/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.airport {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires log4j;
-
-
-    opens com.example.airport to javafx.fxml;
-    exports com.example.airport;
-}
